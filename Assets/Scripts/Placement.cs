56c7dc7a060e89360c74469612c08a5210982c45
jgregory <joseph88888888@gmail.com>
2016-07-26 11:14:19 -0400

    Placement works

---
+++ Assets/Scripts/Placement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Windows.Speech;
+using HoloToolkit.Unity;
+using System;
+
+public class Placement : Singleton<Placement>
+{
+    private bool IsPlaced = false;
+
+    private int MoveDistance = 2;
+
+    void Start()
+    {
+
+    }
+
+    public void InitPlacement()
+    {
+        IsPlaced = false;
+    }
+
+    void Update()
+    {
+        if(!IsPlaced)
+        {
+            //Debug.LogFormat("Enter Update Else");
+            transform.position = Vector3.Lerp(transform.position, ProposeTransformPosition(), 0.2f);
+        }
+    }
+
+    Vector3 ProposeTransformPosition()
+    {
+        // Put the anchor 2m in front of the user.
+        Vector3 retval = Camera.main.transform.position + Camera.main.transform.forward * MoveDistance;
+        transform.rotation = GameObject.Find("Main Camera").transform.rotation;
+
+        return retval;
+    }
+
+    void OnSelect()
+    {
+        IsPlaced = !IsPlaced;
+    }
+
+    /// <summary>
+    /// When a remote system has a transform for us, we'll get it here.
+    /// </summary>
+    /// <param name="msg"></param>
+    //void OnStageTransfrom(NetworkInMessage msg)
+    //{
+    //    // We read the user ID but we don't use it here.
+    //    msg.ReadInt64();
+
+    //    transform.localPosition = CustomMessages.Instance.ReadVector3(msg);
+    //    transform.localRotation = CustomMessages.Instance.ReadQuaternion(msg);
+
+    //    // The first time, we'll want to send the message to the anchor to do its animation and
+    //    // swap its materials.
+    //    if (GotTransform == false)
+    //    {
+    //        GetComponent<InitializeGraph>().Place();
+    //    }
+
+    //    GotTransform = true;
+    //}
+}
--- Assets/Scripts/VoiceCommands.cs
+++ Assets/Scripts/VoiceCommands.cs
@@ -15,27 +15,17 @@ public class VoiceCommands : MonoBehaviour {
     public GameObject GeneratedCube;
     public GameObject GeneratedSphere;
 
+    private GameObject DemoArea;
+
 	// Use this for initialization
 	void Start () {
         _offset = new Vector3(0, 0, 0);
-        GameObject DemoArea = GameObject.Find("DemoArea");
+        DemoArea = GameObject.Find("DemoArea");
+
         _commands = new Dictionary<string, Action>();
-        _commands.Add("Cube", () =>
-        {
-            var genCube = Instantiate(GeneratedCube,
-                DemoArea.transform.position + _offset, Quaternion.identity) as GameObject;
-            genCube.transform.parent = DemoArea.transform;
-            _offset = _offset + new Vector3(2f, 0, 2f);
-            //_generatedList.Add(genCube);
-        });
-        _commands.Add("Sphere", () =>
-        {
-            var genSphere = Instantiate(GeneratedSphere,
-                DemoArea.transform.position + _offset, Quaternion.identity) as GameObject;
-            genSphere.transform.parent = DemoArea.transform;
-            _offset = _offset + new Vector3(2f, 0, 2f);
-            //_generatedList.Add(genSphere);
-        });
+        _commands.Add("Cube", CreateCube);
+        _commands.Add("Sphere", CreateSphere);
+
         _commands.Add("Reset", () =>
         {
             foreach (Transform child in DemoArea.transform)
@@ -49,6 +39,28 @@ public class VoiceCommands : MonoBehaviour {
         _keywordRecognizer.OnPhraseRecognized += CallCommand;
         _keywordRecognizer.Start();
     }
+
+    void CreateShape(GameObject shape)
+    {
+        var genShape = Instantiate(shape,
+    DemoArea.transform.position + _offset, Quaternion.identity) as GameObject;
+        genShape.transform.parent = DemoArea.transform;
+
+        genShape.GetComponent<Placement>().InitPlacement();
+
+        _offset = _offset + new Vector3(2f, 0, 2f);
+    }
+
+    void CreateCube()
+    {
+        CreateShape(GeneratedCube);
+    }
+
+    void CreateSphere()
+    {
+        CreateShape(GeneratedSphere);
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
